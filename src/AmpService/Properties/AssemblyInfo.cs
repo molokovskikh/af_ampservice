42a2b02702c60b9db87442af3c6e4c05ce2f0f30
Kvasov <Kvasov@ee1ff298-451b-584b-9821-aa639ebf7136>
2006-10-10 07:00:00 +0000

    [AMP-238]

--- src/AmpService/Properties/AssemblyInfo.cs
+++ src/AmpService/Properties/AssemblyInfo.cs
@@ -10,4 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("4D22283C-14D7-4803-995D-3FE4B832F9B4")]
-[assembly: AssemblyVersion("1.0.9.0")]
+[assembly: AssemblyVersion("1.0.10.0")]
