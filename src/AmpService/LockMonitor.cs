53ec486a8c91efd157a9e188a542733fa57b2a41
Kvasov <Kvasov@ee1ff298-451b-584b-9821-aa639ebf7136>
2008-04-23 10:36:00 +0000

    Добавлена обработка исключений в мониторе

--- src/AmpService/LockMonitor.cs
+++ src/AmpService/LockMonitor.cs
@@ -4,96 +4,117 @@ using System.Collections.Specialized;
 using System.Threading;
 using System.Web;
 using log4net;
-using MySql.Data.MySqlClient;
 
 namespace AmpService
 {
 	public class LockMonitor
 	{
 		private readonly Thread _monitorThread;
-		private bool _stopped = false;
+		private bool _stopped;
 		private readonly Dictionary<HttpRequest, DateTime> _monitoringRequests = new Dictionary<HttpRequest, DateTime>();
 		private readonly ILog _log = LogManager.GetLogger(typeof (LockMonitor));
 
 		public LockMonitor()
 		{
-			_monitorThread = new Thread(Monitor);
-			_monitorThread.Start();
+			try
+			{
+				_monitorThread = new Thread(Monitor);
+				_monitorThread.Start();
+			}
+			catch(Exception ex)
+			{
+				_log.Error("Ошибка в конструкторе", ex);
+			}
 		}
 
 		public void Add(HttpRequest request)
 		{
-			lock (_monitoringRequests)
-				_monitoringRequests.Add(request, DateTime.Now);
+			try
+			{
+				lock (_monitoringRequests)
+					_monitoringRequests.Add(request, DateTime.Now);
+			}
+			catch(Exception ex)
+			{
+				_log.Error("Ошибка при добавлении запроса монитору", ex);
+			}
 		}
 
 		public void Remove(HttpRequest request)
 		{
-			lock (_monitoringRequests)
-				if (_monitoringRequests.ContainsKey(request))
-					_monitoringRequests.Remove(request);
+			try
+			{
+				lock (_monitoringRequests)
+					if (_monitoringRequests.ContainsKey(request))
+						_monitoringRequests.Remove(request);
+			}
+			catch(Exception ex)
+			{
+				_log.Error("Ошибка при удалении запроса из монитора", ex);
+			}
 		}
 
 		public void Stop()
 		{
-			_stopped = true;
-			_monitorThread.Join(2000);
-			if (_monitorThread.ThreadState != ThreadState.Stopped)
-				_monitorThread.Abort();
+			try
+			{
+				_stopped = true;
+				_monitorThread.Join(2000);
+				if (_monitorThread.ThreadState != ThreadState.Stopped)
+					_monitorThread.Abort();
+			}
+			catch(Exception ex)
+			{
+				_log.Error("Ошибка при остановке монитора", ex);
+			}
 		}
 
 		private void Monitor()
 		{
-			while (!_stopped)
+			try
 			{
-				lock (_monitoringRequests)
+				while (!_stopped)
 				{
-					List<HttpRequest> toNotify = new List<HttpRequest>();
-					foreach (KeyValuePair<HttpRequest, DateTime> pair in _monitoringRequests)
-						if (DateTime.Now - pair.Value > TimeSpan.FromSeconds(10))
-							toNotify.Add(pair.Key);
-
-					if (toNotify.Count > 0)
+					lock (_monitoringRequests)
 					{
-						string innodbStatus;
-						using (MySqlConnection connection = new MySqlConnection(Literals.ConnectionString))
+						var toNotify = new List<HttpRequest>();
+						foreach (var pair in _monitoringRequests)
+							if (DateTime.Now - pair.Value > TimeSpan.FromSeconds(10))
+								toNotify.Add(pair.Key);
+
+						if (toNotify.Count > 0)
 						{
-							connection.Open();
-							MySqlCommand command = connection.CreateCommand();
-							command.CommandText = "show engine innodb status;";
-							using (MySqlDataReader reader = command.ExecuteReader())
+							var requests = "";
+
+							foreach (var request in toNotify)
 							{
-								reader.Read();
-								innodbStatus = (string) reader[2];
+								requests += String.Format("Запрос: {0}\r\n", request.Path);
+								NameValueCollection parameters;
+								if (request.RequestType == "GET")
+									parameters = request.QueryString;
+								else
+									parameters = request.Form;
+								foreach (string key in parameters)
+									requests += String.Format("{0}: {1}\r\n", key, request.Params[key]);
+
+								requests += "\r\n";
 							}
+
+
+							_log.ErrorFormat(@"Следующие запросы ожидают более 10 секунд: 
+{0}
+", requests);
 						}
 
-						string requests = "";
-
-						foreach (HttpRequest request in toNotify)
-						{
-							requests += String.Format("Запрос: {0}\r\n", request.Path);
-							NameValueCollection parameters;
-							if (request.RequestType == "GET")
-								parameters = request.QueryString;
-							else
-								parameters = request.Form;
-							foreach (string key in parameters)
-								requests += String.Format("{0}: {1}\r\n", key, request.Params[key]);
-
-							requests += "\r\n";
-						}
-
-
-						_log.ErrorFormat(@"Следующие запросы ожидают более 10 секунд: 
-{0}{1}
-", requests, innodbStatus);
+						foreach (var request in toNotify)
+							_monitoringRequests.Remove(request);
 					}
-
-					foreach (HttpRequest request in toNotify)
-						_monitoringRequests.Remove(request);
+					Thread.Sleep(1000);
 				}
-				Thread.Sleep(1000);
+			}
+			catch(Exception ex)
+			{
+				_log.Error("Ошибка в работе монтора", ex);
 			}
 		}
 
