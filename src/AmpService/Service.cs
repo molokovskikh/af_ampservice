3f2c5893090e089df80c375644482fa1baf91f70
kvasov <kvasov@ee1ff298-451b-584b-9821-aa639ebf7136>
2009-10-05 13:45:22 +0000

    Исправил ошибку внесенную предыдущим комитом

--- src/AmpService/Service.cs
+++ src/AmpService/Service.cs
@@ -238,7 +238,7 @@ from usersettings.prices p
 
 			var result = new DataSet();
 			var table = result.Tables.Add("Prices");
-			table.Columns.Add("OrderID", typeof (ulong));
+			table.Columns.Add("OrderID", typeof (long));
 			table.Columns.Add("OriginalOrderID", typeof (ulong));
 
 			var selectOffer = @"
