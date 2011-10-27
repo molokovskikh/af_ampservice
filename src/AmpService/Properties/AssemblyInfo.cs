5c2862411cda7d8ae3a1f099f7e2543980684ad8
Квасов Роман <r.kvasov@analit.net>
2011-10-27 10:34:37 +0400

    перевел на nuget

--- src/AmpService/Properties/AssemblyInfo.cs
+++ src/AmpService/Properties/AssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
-[assembly: CLSCompliant(true)]
+[assembly: CLSCompliant(false)]
 [assembly: Guid("4D22283C-14D7-4803-995D-3FE4B832F9B4")]
 [assembly: AssemblyVersion("1.1.17.*")]
