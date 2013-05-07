1a3f1b3f4c6d7aa4e5267edd12d95831a28f304a
Квасов Роман <r.kvasov@analit.net>
2013-05-07 13:53:50 +0400

    Ошибка #16893 Запрос Курина GetOrderItems - добавил группировку

--- src/AmpService/Service.cs
+++ src/AmpService/Service.cs
@@ -622,7 +622,8 @@ WHERE oh.Deleted = 0
 	and oh.ClientCode = ?clientId
 	and oh.WriteTime >= ?begin
 	and oh.WriteTime <= ?end
-order by oh.WriteTime";
+order by oh.WriteTime
+group by ol.RowId";
 
 			return With.Connection(c => {
 				var adapter = new MySqlDataAdapter(sql, c);
