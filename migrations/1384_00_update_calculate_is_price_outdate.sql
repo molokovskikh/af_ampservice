298377162be7efff770c1bc026461a648e950502
Квасов Роман <r.kvasov@analit.net>
2014-10-15 16:15:19 +0400

    Новшество #27777 Обработка цен
    Реализовал вычисления флага "Заказ сделан по не актуальному прайс-листу"
    для многофайловых прайс-листов

---
+++ migrations/1384_00_update_calculate_is_price_outdate.sql
@@ -0,0 +1,18 @@
+drop trigger Orders.CalculateIsPriceOutdate;
+CREATE DEFINER=`RootDBMS`@`127.0.0.1` TRIGGER orders.CalculateIsPriceOutdate
+BEFORE INSERT
+ON orders.ordershead
+FOR EACH ROW
+BEGIN
+	declare localPriceDate datetime;
+
+	select pi.PriceDate
+	into localPriceDate
+	from Customers.Intersection i
+		join UserSettings.PricesCosts pc on pc.CostCode = i.CostId
+			join UserSettings.PriceItems pi on pi.Id = pc.PriceItemId
+	where i.PriceId = NEW.PriceCode and i.RegionId = NEW.RegionCode and i.ClientId = NEW.ClientCode
+	limit 1;
+
+	set NEW.IsPriceOutdate = localPriceDate is null or NEW.PriceDate <> localPriceDate;
+END;
