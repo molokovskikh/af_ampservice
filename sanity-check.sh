e5ca2c19736814fb7ffa8a12aeed8f03ce832025
Квасов Роман <r.kvasov@analit.net>
2014-10-15 14:50:13 +0400

    По просьбе Епихина обновил релизный конфиг

--- sanity-check.sh
+++ sanity-check.sh
@@ -1,4 +1,4 @@
 user=
 read -p "user for ios.analit.net: " user
 read -s -p "password: " password
-curl -i http://$user:$password@ios.analit.net/AmpServ/
+curl -i http://$user:$password@ios.analit.net/AmpServ/core.asmx/GetPrices?onlyLeader=false&newEar=false&limit=10&selStart=0&rangeField=OriginalName&rangeValue=*&sortField=OriginalName&sortOrder=asc
